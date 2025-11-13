6b29bf0dd9c1f8234944dd7bbe60b928b5c1e08c
JHeintz <Joseph.Heintz@vtsd.com>
2025-11-13 11:46:07 -0500

    New stuff

--- 1.1.8/Step 18.py
+++ 1.1.8/Step 18.py
@@ -32,12 +32,26 @@ for s in turtle_shapes:
     vt.setheading(270)
 
     tloc += 50
-
+distance = 3
+pixel_size = 20
 # TODO: move turtles across and down screen, stopping for collisions
-"""
+
 for step in range(50):
-	# do something
-"""
+    # For each vertical tutle
+    for vt in vert_turtles:
+        for ht in  horiz_turtles:
+            ht.forward(distance)
+            vt.forward(distance)
+            distance +=1
+            if distance > 6:
+                distance -=1
+            if abs(ht.xcor() - vt.xcor()) < pixel_size:
+                if abs(ht.ycor() - vt.ycor()) < pixel_size:
+                    ht.fillcolor("grey")
+                    horiz_turtles.remove(ht)
+                    vt.fillcolor("grey")
+                    vert_turtles.remove(vt)
+
 
 wn = trtl.Screen()
 wn.mainloop()
