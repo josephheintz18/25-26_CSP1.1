213aa46322fd7dcf19bbbb9588f7f1ff213972aa
JHeintz <Joseph.Heintz@vtsd.com>
2025-11-20 13:51:35 -0500

    New stuff

--- 1.1.9/TheProjectforthisdirectory.py
+++ 1.1.9/TheProjectforthisdirectory.py
@@ -1,8 +1,9 @@
 import turtle as trtl
 t = trtl.Turtle()
+t.speed(0)
 
 lamp = 0
-t.speed(0)
+
 #Cap
 t.right(45)
 t.penup()
@@ -27,11 +28,34 @@ for a in range(2):
         t.pendown()
         t.forward(160)
     lamp +=5
+t.penup()
+t.goto(-56,-96)
+t.right(-206)
+t.pendown()
+t.circle(-97, -99)
 
 #Lamp Base
-t.goto(-32,-120)
-t.right(-154)
+t.penup()
+t.goto(-56,-96)
+t.pendown()
+t.right(-53)
+t.forward(65)
+t.right(30)
+#yk yk
+t.right(-107)
+t.forward(150)
+t.penup()
+t.forward(-150)
+t.right(107)
+#yk yk
+t.pendown()
+t.forward(65)
+t.right(-107)
+t.pendown()
+t.right(-130)
 t.circle(-100, -100)
+t.right(135)
+t.forward(65)
 
 wn = trtl.Screen()
 wn.mainloop()
